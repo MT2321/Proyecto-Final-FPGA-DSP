6427b7ac016c4d187eeefa71a31201b94ef472ce
Ian Cruz Diaz <idiaz@itba.edu.ar>
2022-09-06 22:40:35 -0300

    Graphiker seems to be working on TEST IAN

--- test_ian/pixel_discriminator.v
+++ test_ian/pixel_discriminator.v
@@ -2,15 +2,19 @@ module pixel_discriminator(value, pixel_y, out_pixel);
 	input [11:0] value;
 	input [9:0] pixel_y;
 	output reg out_pixel;
-	reg y_value;
+	reg signed[9:0] y_value;
 	initial begin
 		out_pixel <= 0;
 		y_value <= 0;
 	end
 	localparam division_const = 7;
+	localparam signed screen_height = 500;
+	localparam signed trace_width = 5;
 	always @(*) begin
-		y_value = 600 - pixel_y;
-		if(((value/7)+300) > pixel_y && ((value/7)+298) < pixel_y) begin
+	
+		y_value = screen_height - pixel_y;
+		
+		if((value / division_const) > y_value && (value / division_const) < y_value + 5 ) begin
 			out_pixel = 1;
 		end
 		else begin
