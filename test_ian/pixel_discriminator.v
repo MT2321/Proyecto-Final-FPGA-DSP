1e0e55ec606609b72b17f43e5af796f71d861a0e
Ian Cruz Diaz <idiaz@itba.edu.ar>
2022-11-17 21:27:37 -0300

    test: Verificado por simulación Graphiker
    Ya se verifico por simulación el Graphiker, insertando una config
    especial en pixel_discriminator.v (en el codigo esta comentado que es
    lo que se cambio para poder probarlo) y la simulación se encuentra en
    Waveform5.vwf.

--- test_ian/pixel_discriminator.v
+++ test_ian/pixel_discriminator.v
@@ -7,20 +7,23 @@ module pixel_discriminator(value, pixel_y, out_pixel);
 		out_pixel <= 0;
 		y_value <= 0;
 	end
-	localparam division_const = 7;			// Constante de division surge
+	// Ahora se lo cambio a 6 para poder hacer una prueba facilmente con Quartus para que me de el resultado esperado
+	// 4095/6 = 682.5, si selecciono una trace_width en 83, en la primera linea de la pantalla se me prende el pixel y
+	// ya de la segunda en adelante se me apaga.
+	localparam division_const = 6;			// Constante de division surge
 											// de la relacion entre el numero
 											// de pixeles verticales en la imagen (600) y los 
 											// valores máximos de entrada (12 bits = 4096)
 											// 4096/600 = 6.82 -> 7
 	localparam signed screen_height = 600; 	// Altura de la pantalla	
-	localparam signed trace_width = 5;		// Ancho de la traza en la pantalla (para que no sea una linea de un pixel)
+	localparam signed trace_width = 84;		// Ancho de la traza en la pantalla (para que no sea una linea de un pixel)
 	always @(*) begin
 	
 		y_value = screen_height - pixel_y;	// Se invierte el valor de la posicion vertical del pixel
 											// para que el pixel mas alto de la imagen tenga el valor 0
 											// y el mas bajo el valor 600
 		
-		if((value / division_const) > y_value && (value / division_const) < y_value + 5 ) begin
+		if((value / division_const) > y_value && (value / division_const) < y_value + trace_width ) begin
 			out_pixel = 1;				// Si el valor de entrada es mayor que el valor de la posicion vertical del pixel
 											// y menor que el valor de la posicion vertical del pixel + 5, se activa el pixel
 		end
