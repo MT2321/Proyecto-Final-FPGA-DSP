06671328b92e210b223f8e870edb1708881e7223
Ian Cruz Diaz <idiaz@itba.edu.ar>
2022-11-17 21:29:44 -0300

    fix: final pixel_discriminator version
    Se eliminaron los parametros cambiados para el test de
    pixel_discriminator y se pusieron los parametros por defecto que deben
    ir para que en la realidad funcione bien.

--- test_ian/pixel_discriminator.v
+++ test_ian/pixel_discriminator.v
@@ -7,16 +7,13 @@ module pixel_discriminator(value, pixel_y, out_pixel);
 		out_pixel <= 0;
 		y_value <= 0;
 	end
-	// Ahora se lo cambio a 6 para poder hacer una prueba facilmente con Quartus para que me de el resultado esperado
-	// 4095/6 = 682.5, si selecciono una trace_width en 83, en la primera linea de la pantalla se me prende el pixel y
-	// ya de la segunda en adelante se me apaga.
-	localparam division_const = 6;			// Constante de division surge
+	localparam division_const = 7;			// Constante de division surge
 											// de la relacion entre el numero
 											// de pixeles verticales en la imagen (600) y los 
 											// valores máximos de entrada (12 bits = 4096)
 											// 4096/600 = 6.82 -> 7
 	localparam signed screen_height = 600; 	// Altura de la pantalla	
-	localparam signed trace_width = 84;		// Ancho de la traza en la pantalla (para que no sea una linea de un pixel)
+	localparam signed trace_width = 5;		// Ancho de la traza en la pantalla (para que no sea una linea de un pixel)
 	always @(*) begin
 	
 		y_value = screen_height - pixel_y;	// Se invierte el valor de la posicion vertical del pixel
