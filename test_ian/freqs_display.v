123f5fb91320dd6cd659b14bbc3410e55b5da244
Ian Cruz Diaz <idiaz@itba.edu.ar>
2023-01-20 15:05:22 -0300

    feat: Add freqs_display block pot capability Al modulo de freqs_display se le agrega la capacidad de setear un prescaler para ajustar cada cuantas muestras se actualiza el valor del modulo de potencia. Esto se hace para evitar que el modulo de potencia tenga que guardar todas las muestras en un buffer.

--- test_ian/freqs_display.v
+++ test_ian/freqs_display.v
@@ -1,4 +1,4 @@
-module freqs_display(freq_value, posx, clk, val_out, freq_pos_needed);
+module freqs_display(freq_value, posx, clk, prescaler, set_values_flag, val_out, freq_pos_needed);
 /* Este modulo se encarga de mostrar un boometro en representacion de la FFT.
     Recibe un valor de energía de la FFT para un bin de frecuencias dado y lo muestra en 
     la pantalla.
@@ -8,17 +8,26 @@ module freqs_display(freq_value, posx, clk, val_out, freq_pos_needed);
     input [11:0] freq_value;             // input value
     input [9:0] posx;                    // Posicion horizontal de la pantalla
     input clk;                           // Clock
+    input [15:0] prescaler;              // prescaler para ajustar cada cuantas muestras se calcula la potencia de cada banda
+
+    output reg set_values_flag;          // Flag para mandarle al calculador de potencia que actualize los valores de potencia
     output reg [11:0] val_out;           // output value
     output reg [7:0] freq_pos_needed;    // Que bin de frecuencia se necesita mostrar en la proxima muestra
 
+    reg [15:0] counter;                 // Contador para el prescaler
+
     localparam screen_width = 800;      // Ancho de la pantalla
     localparam screen_height = 600;     // Altura de la pantalla
-
     localparam amount_of_bins = 10;     // Cantidad de bins de frecuencias
     localparam bin_width = screen_width / amount_of_bins; // Ancho de cada bin de frecuencias
 
+    initial begin
+        set_values_flag <= 0;           // inicializamos el flag en 0
+        counter <= 0;                   // inicializamos el contador
+	end
+
 	always @(posedge clk) begin
-        // Asumo que hay solo0 10 bines de frecuencias
+        // Asumo que hay solo 10 bines de frecuencias
         if (posx < bin_width) begin
             freq_pos_needed <= 0;
         end
@@ -53,5 +62,15 @@ module freqs_display(freq_value, posx, clk, val_out, freq_pos_needed);
             freq_pos_needed <= 0;
         end
         val_out <= freq_value;
+
+
+        if (counter == prescaler) begin // si el contador llega al valor del prescaler
+            set_values_flag <= 1;       // mandamos el flag
+            counter <= 0;               // reiniciamos el contador
+        end
+        else begin                      // si no
+            counter <= counter + 1;     // incrementamos el contador
+            set_values_flag <= 0;       // no mandamos el flag
+        end
 	end
 endmodule
