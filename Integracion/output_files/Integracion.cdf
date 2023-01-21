e2baaf46687a25385555acace27a05a17a1a073f
Martin.Turco <mrodriguez@itba.edu.ar>
2023-01-21 20:36:10 -0300

    Testing Integracion Roll

---
+++ Integracion/output_files/Integracion.cdf
@@ -0,0 +1,13 @@
+/* Quartus Prime Version 20.1.1 Build 720 11/11/2020 SJ Lite Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(EP4CE22F17) Path("C:/Users/mrspa/Documents/Repos/ME/Proyecto-Final-FPGA-DSP/Integracion/output_files/") File("Integracion.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
--- Integracion/source/roll.v
+++ Integracion/source/roll.v
@@ -1,16 +1,20 @@
-module roll(value, clk, prescaler, val_out, w_clk);
+module roll(value, clk, val_out, w_clk, one);
 /* Este modulo se encarga de mandar mas lentamente a la pantalla una señal en modo roll
 */
 	input [11:0] value;			        // input value
     input clk;                          // clock
-    input [15:0] prescaler;             // prescaler para ajustar el tiempo de roll desde real-time a 1/2^15 de real-time
+    //input [15:0] prescaler;             // prescaler para ajustar el tiempo de roll desde real-time a 1/2^15 de real-time
     output reg [11:0] val_out;          // output value
     output reg w_clk;                   // clock de escritura en la pantalla
+	 output reg one;
     reg [15:0] counter;
+	 
+	 localparam prescaler = 1;
 
 	initial begin
         counter <= 0;                   // inicializamos el contador
         w_clk <= 0;                     // inicializamos el clock de escritura
+		  one <=1;
 	end
 
 	always @(posedge clk) begin
